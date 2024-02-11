f48e581b8a4273cd4edcc6e10e5445558c9bd348
Samiksh6 <samikshbansod6@gmail.com>
2024-02-12 00:28:26 +0530

    feat: added java file:

---
+++ demo1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Even_odd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+				Scanner sc=new Scanner(System.in);
+				int num;
+				System.out.print("Enter one Number : ");
+				num=sc.nextInt();
+				
+				if(num%2==0)
+					System.out.print("Number is Even");
+				else
+					System.out.print("Number is Odd");
+				sc.close();
+			}
+
+		}
+
+
